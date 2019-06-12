829963de04e219b4510ae6575fc876cb7f7d9da1
mariano.barraco <mariano.barraco@avature.net>
2019-06-12 00:07:55 -0300

    First commit

---
+++ app.py
@@ -0,0 +1,36 @@
+from flask import (
+    Flask,
+    render_template,
+    request
+)
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def home():
+    return(' Hola wach')
+
+
+@app.route('/anova_01', methods=['GET', 'POST'])
+def anova_01():
+    return render_template('anova_01.html')
+
+
+@app.route('/anova_02', methods=['GET', 'POST'])
+def anova_02():
+    return render_template('anova_02.html')
+
+
+@app.route('/anova_03', methods=['GET', 'POST'])
+def anova_03():
+    return render_template('anova_03.html')
+
+
+@app.route('/tp-4-5', methods=['GET', 'POST'])
+def tp_4_5():
+    return render_template('tp_4.5.html')
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5999, debug=True)
